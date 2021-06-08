a41a3ba15b2c25d5df813663e50daeda0beed064
ribeirodg <daianeribeiro2@yahoo.com.br>
2021-06-07 21:25:40 -0300

    Aula4

---
+++ index.js
@@ -0,0 +1,39 @@
+let nome = 'Daiane';
+let idade = 34;
+let maiorDeIdade = true;
+
+let pessoa = {
+    nome: nome,
+    idade: idade,
+    maiorDeIdade: maiorDeIdade
+}
+
+// console.log(pessoa.nome)
+
+let  animais = ['macaco', 'papagaio', 'cobra', 'elefante']
+
+let numero1 = 10
+let numero2 = 20
+
+let resultadoSoma = numero1 + numero2
+console.log ('O resultado da soma é ' + resultadoSoma)
+
+let resultadoSubtracao = numero1 - numero2
+console.log ('O resultado da subtração é ' + resultadoSubtracao)
+
+let resultadoDivisao = numero1 / numero2
+console.log ('O resultado da Divisão é ' + resultadoDivisao)
+
+let resultadoMultiplicacao = numero1 * numero2
+console.log ('O resultado da multiplicação é ' + resultadoMultiplicacao)
+
+if (numero1 > numero2) {
+    console.log ('O' + numero1 + ' é maior que o ' + numero2)
+}
+else if {
+    console.log ('O' + numero2 + ' é maior que o ' + numero1)
+}
+
+else {
+    console.log ('Os números são iguais')
+}
